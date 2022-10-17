420d4f8b9f99a3810aae6386efd582314c6f0c91
Aaron Thomas <aaron.thomas231@yahoo.comgit>
2022-10-16 18:31:14 -0700

    Adding movement with Arrow keys

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const map = document.querySelector(".map");
   const jumper = document.createElement("div");
   let jumperLeftSpace = 50;
-  let jumperBottomSpace = 150;
+  let jumperBottomSpace = 250;
   let isGameOver = false;
   let platformCount = 5;
+  let platforms = [];
+  let upTimerId;
+  let downTimerId;
+  let isJumping = true;
 
   function createJumper() {
     map.appendChild(jumper);
     jumper.classList.add("jumper");
+    jumperLeftSpace = platforms[0].left;
     jumper.style.left = jumperLeftSpace + "px";
     jumper.style.bottom = jumperBottomSpace + "px";
   }
@@ -36,10 +41,75 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  function movePlatforms() {
+    if (jumperBottomSpace > 200) {
+      platforms.forEach((platform) => {
+        platform.bottom -= 4;
+        let visual = platform.visual;
+        visual.style.bottom = platform.bottom + "px";
+      });
+    }
+  }
+
+  function jump() {
+    clearInterval(downTimerId);
+    isJumping = true;
+    upTimerId = setInterval(function () {
+      jumperBottomSpace += 20;
+      jumper.style.bottom = jumperBottomSpace + "px";
+      if (jumperBottomSpace > 350) {
+        fall();
+      }
+    }, 30);
+  }
+
+  function fall() {
+    clearInterval(upTimerId);
+    isJumping = false;
+    downTimerId = setInterval(function () {
+      jumperBottomSpace -= 5;
+      jumper.style.bottom = jumperBottomSpace + "px";
+      if (jumperBottomSpace <= 0) {
+        gameOver();
+      }
+      platforms.forEach((platform) => {
+        if (
+          jumperBottomSpace >= platform.bottom &&
+          jumperBottomSpace <= platform.bottom + 15 &&
+          jumperLeftSpace + 60 >= platform.left &&
+          jumperLeftSpace <= platform.left + 85 &&
+          !isJumping
+        ) {
+          console.log("landed");
+          jump();
+        }
+      });
+    }, 30);
+  }
+
+  function gameOver() {
+    console.log("game over");
+    isGameOver = true;
+    clearInterval(upTimerId);
+    clearInterval(downTimerId);
+  }
+
+  function control(e) {
+    if (e.key === "ArrowLeft") {
+      // Move Jumper Left
+    } else if (e.key === "ArrowRight") {
+      //Move Jumper Right
+    } else if (e.key === "ArrowUp") {
+      //moveStraight
+    }
+  }
+
   function start() {
     if (isGameOver == false) {
-      createJumper();
       createPlatforms();
+      createJumper();
+      setInterval(movePlatforms, 30);
+      jump();
     }
   }
   start();
